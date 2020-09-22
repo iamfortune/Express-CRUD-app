8023a12be5f0e9087d2f545ac55962dcd5dcc05e
iamfortune <ikechifortune@gmail.com>
2020-09-22 16:00:39 +0100

    currently refactoring code to accept data from mongoDB

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,4 @@
-const fs = require('fs');
 const Tour = require('./../models/tourModel');
-
 
 exports.checkID = (req, res, next, val) => {
   console.log(`Tour id is ${val}`);
@@ -28,11 +26,11 @@ exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
-    requestedAt: req.requestTime,
-    results: tours.length,
-    data: {
-      tours: tours
-    }
+    requestedAt: req.requestTime
+    // results: tours.length,
+    // data: {
+    //   tours: tours
+    // }
   });
 };
 
@@ -41,33 +39,23 @@ exports.getTour = (req, res) => {
 
   const id = req.params.id * 1; // we converted the id string to number
 
-  const tour = tours.find(
-    el => el.id === id
-  ); /** here we loop through the tours and try to find the element whose id matches the one we are looking for */
+  // const tour = tours.find(
+  //   el => el.id === id
+  // ); /** here we loop through the tours and try to find the element whose id matches the one we are looking for */
 
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour
-    }
-  });
+  // res.status(200).json({
+  //   status: 'success',
+  //   data: {
+  //     tour
+  //   }
+  // });
 };
 
 exports.createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    err => {
-      res.status(201).json({
-        status: 'success',
-        data: { tour: newTour }
-      });
-    }
-  );
+  res.status(201).json({
+    status: 'success'
+    // data: { tour: newTour }
+  });
 };
 
 exports.updateTour = (req, res) => {
