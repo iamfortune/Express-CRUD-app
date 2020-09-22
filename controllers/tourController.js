3330462a09a71e1610b32a4cfd1bd6b3389f8e53
iamfortune <ikechifortune@gmail.com>
2020-09-22 16:21:41 +0100

    added more refactors on code

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,16 +1,5 @@
 const Tour = require('./../models/tourModel');
 
-exports.checkID = (req, res, next, val) => {
-  console.log(`Tour id is ${val}`);
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID'
-    });
-  }
-  next();
-};
-
 exports.checkBody = (req, res, next) => {
   if (!req.body.name || !req.body.price) {
     return res.status(400).json({
