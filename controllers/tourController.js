845531d535352c6d4a222ea9b76138d17bafd9a8
iamfortune <ikechifortune@gmail.com>
2020-10-14 15:51:42 +0100

    fixed bugs and added a little more files

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -58,11 +58,16 @@ exports.createTour = async (req, res) => {
 
 exports.updateTour = async (req, res) => {
   try {
-    Tour.findByIdAndUpdate(req.params.id)
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
 
     res.status(200).json({
       status: 'success',
-      data: { tour: '<Updated tour ....>' }
+      data: {
+        tour
+      }
     });
   } catch (error) {
     res.status(404).json({
@@ -72,9 +77,18 @@ exports.updateTour = async (req, res) => {
   }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({
+      status: 'success',
+      data: null
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'fail',
+      message: error
+    });
+  }
 };
--- server.js
+++ server.js
@@ -21,7 +21,7 @@ mongoose
   });
 
 // STARTING SERVER
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`app running on port ${port}`);
 });
