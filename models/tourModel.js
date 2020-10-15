1129cec4a4dd3fcc8241d61f78c92ef4933f3510
iamfortune <ikechifortune@gmail.com>
2020-10-15 10:48:42 +0100

    added models and schema for the tours

--- models/tourModel.js
+++ models/tourModel.js
@@ -4,15 +4,37 @@ const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'A tour name is required'],
-    unique: true
+    unique: true,
+    trim: true
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'Tour must have a duration']
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a maximum group size']
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'Please add difficulty']
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0
   },
   price: {
     type: Number,
     required: [true, 'A tour price is required']
+  },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true
   }
 });
 const Tour = mongoose.model('Tour', tourSchema);
