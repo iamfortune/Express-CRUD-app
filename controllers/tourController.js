e3f6194bdecd94c4ce8f0e7790f8909736c399f6
iamfortune <ikechifortune@gmail.com>
2020-11-03 12:21:49 +0100

    added sorting to the API

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,22 +4,29 @@ const Tour = require('./../models/tourModel');
 // ROUTE HANDLERS
 exports.getAllTours = async (req, res) => {
   try {
+    console.log(req.query);
     // BUILD QUERY
     // FILTERING QUERY
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach(el => delete queryObj[el]);
 
-    const query = Tour.find(queryObj);
+    // ADVANCED FILTERING
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
 
-    console.log(req.query);
+    let query = Tour.find(JSON.parse(queryStr));
 
-    // { difficulty: 'easy', duration: { $gte: 5 } }
+    // SORTING
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
 
     // EXECUTE QUERY
     const tours = await query;
-
-    // an advanced filter  { difficulty: 'easy', duration: {$gte: 5}}
 
     // SEND RESPONSE
     res.status(200).json({
