c03368dc28e9567d884ef29512265a1bd83f631f
iamfortune <ikechifortune@gmail.com>
2020-12-17 00:16:14 +0100

    made the middleware and CRUD operations reusable using the API Feature class

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,12 +1,20 @@
 const Tour = require('./../models/tourModel');
 
-// ROUTE HANDLERS
-exports.getAllTours = async (req, res) => {
-  try {
-    console.log(req.query);
-    // BUILD QUERY
-    // FILTERING QUERY
-    const queryObj = { ...req.query };
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
+};
+
+class APIFeatures {
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
+
+  filter() {
+    const queryObj = { ...this.queryString };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach(el => delete queryObj[el]);
 
@@ -14,15 +22,38 @@ exports.getAllTours = async (req, res) => {
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
 
-    let query = Tour.find(JSON.parse(queryStr));
+    this.query.find(JSON.parse(queryStr));
 
-    // SORTING
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      query = query.sort(sortBy);
+    return this;
+  }
+
+  sort() {
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.split(',').join(' ');
+      this.query = this.query.sort(sortBy);
     } else {
-      query = query.sort('-createdAt');
+      this.query = this.query.sort('-createdAt');
     }
+
+    return this;
+  }
+
+  limitFields() {
+    if (this.queryString.fields) {
+      const fields = this.query.fields.split(',').join(' ');
+      this.query = this.query.select(fields);
+    } else {
+      this.query = this.query.select('-__v');
+    }
+
+    return this;
+  }
+}
+
+// ROUTE HANDLERS
+exports.getAllTours = async (req, res) => {
+  try {
+    console.log(req.query);
 
     // FIELD LIMITING
     if (req.query.fields) {
@@ -32,8 +63,21 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v');
     }
 
+    // PAGINATION
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('Page does not exist');
+    }
+
     // EXECUTE QUERY
-    const tours = await query;
+    const features = new APIFeatures(Tour.find(), req.query).filter().sort();
+    const tours = await features.query;
 
     // SEND RESPONSE
     res.status(200).json({
@@ -44,7 +88,7 @@ exports.getAllTours = async (req, res) => {
       }
     });
   } catch (error) {
-    res.send(404).json({
+    res.sendStatus(404).json({
       status: 'failed',
       message: error
     });
